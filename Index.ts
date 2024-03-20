966ee46d1311a533c3fc2b3d0e854644e74fd5d9
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-03-21 01:50:20 +0500

    Simple Calculator with Switch code

---
+++ Index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {message: "Enter first number", type: "number", name: "firstname"},
+    {message: "Enter Second number", type: "number", name: "secondname"},
+    {
+        message: "Select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition","Subtraction","Multiplication","Division"]
+    },
+]);
+
+// conditional statment
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstname + answer.secondname)
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.firstname - answer.secondname)
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.firstname * answer.secondname)
+} else if (answer.operator === "Division") {
+    console.log(answer.firstname / answer.secondname)
+} else {
+    console.log("Please select valid operator")
+}
